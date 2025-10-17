14f1229530e3cf8e007f787f810a037a0a09ec6a
ROMERO LLUMIQUINGA <romromllu@a1-16.infor.caminas>
2025-10-17 17:03:19 +0200

    rating bar

--- app/src/main/java/com/example/coffeeshops/MainActivity.kt
+++ app/src/main/java/com/example/coffeeshops/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,6 +19,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -28,6 +32,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,6 +53,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
 import com.example.coffeeshops.ui.theme.CoffeeShopsTheme
 import com.example.coffeeshops.ui.theme.Comments
+import kotlin.math.floor
 
 val courgetteFontFamily = FontFamily(
     Font(R.font.aliviaregular, FontWeight.Normal)
@@ -74,6 +83,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CoffeeMain() {
+    var starsSelected by remember { mutableStateOf(0.0) }
+
     Column(modifier = Modifier.fillMaxWidth().padding(14.dp)) {
         Image(
             painter = painterResource(id = R.drawable.images),
@@ -89,9 +100,40 @@ fun CoffeeMain() {
             text = "Antico Calle Greco",
             fontFamily = courgetteFontFamily
         )
+
+        Spacer(modifier = Modifier.size(10.dp))
+
+        RatingBar(rating = starsSelected, onItemSelected = { newValue -> starsSelected = newValue })
     }
 
 
+}
+
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Double = 0.0,
+    stars: Int = 5,
+    starsColor: Color = Color(0xFF888888),
+    onItemSelected: (Double) -> Unit
+) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        for (i in 1..stars) {
+            val icon = if (i <= floor(rating).toInt()) {
+                Icons.Filled.Star
+            } else {
+                Icons.Outlined.Star
+            }
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+                tint = if (i <= rating) Color(0xFF379665) else starsColor,
+                modifier = Modifier
+                    .clickable { onItemSelected(i.toDouble()) }
+                    .padding(2.dp)
+            )
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
